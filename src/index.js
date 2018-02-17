81f9a2e860b0b14fbcd80ca673afb9236b78d81b
pedsm <peumendonca@gmail.com>
2018-02-17 13:58:03 +0000

    lint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends React.Component { render() {
+class App extends React.Component {
+    render() {
         return (
             <div>
                 <h1>PintNow</h1>
