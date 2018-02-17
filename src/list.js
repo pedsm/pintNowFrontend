6c061781df889e7ba01202fdc1246861571259b1
pedsm <peumendonca@gmail.com>
2018-02-17 19:00:41 +0000

    Added PWA

--- src/list.js
+++ src/list.js
@@ -5,24 +5,10 @@ import Icon from 'material-ui/Icon';
 import Divider from 'material-ui/Divider';
 
 export default class PubList extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            "pubs": [{ "name": "Anchor Inn", "address": "Upper Street, Stratford St Mary, COLCHESTER, Essex", "postcode": "CO7 6LW", "latitude": "51.970390", "longitude": "0.979328" },
-            { "name": "Angel Inn", "address": "Egremont Street, Glemsford, SUDBURY, Suffolk", "postcode": "CO10 7SA", "latitude": "52.094427", "longitude": "0.668408" },
-            { "name": "Black Boy Hotel", "address": "7 Market Hill, SUDBURY, Suffolk", "postcode": "CO10 2EA", "latitude": "52.038683", "longitude": "0.730226" },
-            { "name": "Black Horse", "address": "Lower Street, Stratford St Mary, COLCHESTER, Essex", "postcode": "CO7 6JS", "latitude": "51.966211", "longitude": "0.972091" },
-            { "name": "Angel Inn", "address": "Egremont Street, Glemsford, SUDBURY, Suffolk", "postcode": "CO10 7SA", "latitude": "52.094427", "longitude": "0.668408" },
-            { "name": "Black Boy Hotel", "address": "7 Market Hill, SUDBURY, Suffolk", "postcode": "CO10 2EA", "latitude": "52.038683", "longitude": "0.730226" },
-            { "name": "Black Horse", "address": "Lower Street, Stratford St Mary, COLCHESTER, Essex", "postcode": "CO7 6JS", "latitude": "51.966211", "longitude": "0.972091" },
-            ]
-        }
-    }
-
     render() {
         return (
             <List style={styles.main}>
-                {this.state.pubs.map((pub, i) => (
+                {this.props.pubs.map((pub, i) => (
                     <Fragment key={i}>
                         <ListItem>
                             <Avatar>
