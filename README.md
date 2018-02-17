9aa772d00ef7b8fb0c61f6423d8fd7aa2048b96e
pedsm <peumendonca@gmail.com>
2018-02-17 14:23:42 +0000

    Added readme and layout

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pintNowFrontend
 The pint now frontend
+
+## Get started
+
+Run `npm run dev` to start webpack in watch mode, this will compile the js into the bundle. Also make sure that you have `npm run serve` in order to serve the files. These are just development commands
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Map from './map'
+import List from './list'
 
-class App extends React.Component {
+class App extends React.Component { 
+    constructor(props) {
+        super(props)
+    }
     render() {
         return (
             <div>
-                <h1>PintNow</h1>
+                <header>
+                    <h1>PintNow</h1>
+                </header>
+                <Map />
+                <List data={[]}/>
             </div>
         )
     }
