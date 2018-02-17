a7444e05dca8080d6594350042cca4200a936ea4
pedsm <peumendonca@gmail.com>
2018-02-17 23:19:33 +0000

    Google maps basics

--- src/index.js
+++ src/index.js
@@ -9,13 +9,37 @@ class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            "pubs": [{ "name": "Anchor Inn", "address": "Upper Street, Stratford St Mary, COLCHESTER, Essex", "postcode": "CO7 6LW", "latitude": "51.970390", "longitude": "0.979328" },
-            { "name": "Angel Inn", "address": "Egremont Street, Glemsford, SUDBURY, Suffolk", "postcode": "CO10 7SA", "latitude": "52.094427", "longitude": "0.668408" },
-            { "name": "Black Boy Hotel", "address": "7 Market Hill, SUDBURY, Suffolk", "postcode": "CO10 2EA", "latitude": "52.038683", "longitude": "0.730226" },
-            { "name": "Black Horse", "address": "Lower Street, Stratford St Mary, COLCHESTER, Essex", "postcode": "CO7 6JS", "latitude": "51.966211", "longitude": "0.972091" },
-            { "name": "Angel Inn", "address": "Egremont Street, Glemsford, SUDBURY, Suffolk", "postcode": "CO10 7SA", "latitude": "52.094427", "longitude": "0.668408" },
-            { "name": "Black Boy Hotel", "address": "7 Market Hill, SUDBURY, Suffolk", "postcode": "CO10 2EA", "latitude": "52.038683", "longitude": "0.730226" },
-            { "name": "Black Horse", "address": "Lower Street, Stratford St Mary, COLCHESTER, Essex", "postcode": "CO7 6JS", "latitude": "51.966211", "longitude": "0.972091" }, ]
+            "pubs": [], 
+            position: {
+                lat: 0,
+                lon: 0
+            }
+        }
+    }
+    componentDidMount() {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const {latitude, longitude} = position.coords
+                this.setState((prev,props) => {
+                    const state = Object.assign(prev)
+                    state.position = {
+                        lat: latitude,
+                        lon: longitude
+                    }
+                    fetch(`http://localhost:8080/pubs?lat=${latitude}&lon=${longitude}`)
+                        .then(data => {
+                            console.log(data)
+                            return data.json()
+                        })
+                        .then((json) => {
+                            console.log(json)
+                        })
+                        .catch(err => console.error(err))
+                    return state
+                })
+              });
+        } else {
+            /* geolocation IS NOT available */
         }
     }
     render() {
@@ -26,19 +50,19 @@ class App extends React.Component {
                         <h1>PintNow</h1>
                     </Toolbar>
                 </AppBar>
-                <Map pubs={this.state.pubs} />
-                <List pubs={this.state.pubs}/>
+                <Map position={this.state.position} pubs={this.state.pubs} />
+                <List position={this.state.position} pubs={this.state.pubs} />
             </Fragment>
         )
     }
 }
 
-ReactDOM.render( <App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
 
-if('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
     window.addEventListener('load', () =>
-    navigator.serviceWorker.register('serviceWorker.js')
-      .then(registration => console.log('Service Worker registered'))
-      .catch(err => 'SW registration failed'));
+        navigator.serviceWorker.register('serviceWorker.js')
+            .then(registration => console.log('Service Worker registered'))
+            .catch(err => 'SW registration failed'));
 }
--- src/map.js
+++ src/map.js
@@ -1,22 +1,20 @@
 import React from 'react'
-import Markers from './markers'
-import { Map, Scene } from 'react-arcgis';
-
-
+import { withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 export default class CustomMap extends React.Component {
     render() {
+        const Map = withGoogleMap((props) =>
+            <GoogleMap
+                defaultZoom={8}
+                defaultCenter={{ lat: -34.397, lng: 150.644 }}
+            >
+            </GoogleMap>)
         return (
-            <div style={{ width: '100%', height: 400 }}>
-                <Map
-                    viewProperties={{
-                        center: [-1.554717, 52.37777],
-                        zoom: 6
-                    }}
-                >
-                    <Markers pubs={this.props.pubs} />
-                </Map>
-            </div>
+            <Map
+                containerElement={<div id="map" style={{ width: '100%', height: 400 }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+            />
         )
     }
 }
+                // googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBwMLCQoQ87aUbk-r7HXxtVXnTnx0ocnuY&callback=initMap"
