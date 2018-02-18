6b409f4e8bb136da845034e0f30633eababf3f5d
pedsm <peumendonca@gmail.com>
2018-02-18 00:39:31 +0000

    removed clustering

--- src/map.js
+++ src/map.js
@@ -1,25 +1,28 @@
 import React from 'react'
-import { withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { withGoogleMap, GoogleMap, Marker, MarkerClusterer } from 'react-google-maps'
+import { compose, withHandlers, withProps } from 'recompose'
 
 export default class CustomMap extends React.Component {
     render() {
-        const {pubs, position} = this.props
-        const Map = withGoogleMap((props) =>
+        const { pubs, position } = this.props
+        const Map = compose(
+            withGoogleMap,
+        )(props =>
             <GoogleMap
                 defaultZoom={14}
-                defaultCenter={{ lat: position.lat, lng: position.lon}}
+                defaultCenter={{ lat: position.lat, lng: position.lon }}
             >
-                {pubs.map((pub, i) => {
-                    const pos = pub.location.split(',').map(a => parseFloat(a))
-                    const objPos = {lat: pos[0], lng: pos[1]} 
-                    return <Marker 
-                        position={objPos} 
-                        title={pub.name} 
-                        clickable={true}
-                        key={i} 
+                    {pubs.map((pub, i) => {
+                        const pos = pub.location.split(',').map(a => parseFloat(a))
+                        const objPos = { lat: pos[0], lng: pos[1] }
+                        return <Marker
+                            position={objPos}
+                            title={pub.name}
+                            clickable={true}
+                            key={i}
                         />
-                })}
-            </GoogleMap>)
+                    })}
+                </GoogleMap>)
         return (
             <Map
                 containerElement={<div id="map" style={{ width: '100%', height: 400 }} />}
